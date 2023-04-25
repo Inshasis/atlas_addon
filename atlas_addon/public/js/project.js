1ac58d85b2e7a823193b646b321c6126f1f5cdd9
Hidayat ali <110670154+amafhhinfotech@users.noreply.github.com>
2023-04-26 00:58:41 +0530

    Update project.js

--- atlas_addon/public/js/project.js
+++ atlas_addon/public/js/project.js
@@ -29,8 +29,42 @@ function refresh_location(frm){
     cur_frm.refresh_field('area');
     cur_frm.refresh_field('sub_area');
 }
+frappe.ui.form.on("Project", {
+    plate_no(frm) {
+    if(cur_frm.doc.province && cur_frm.doc.city && cur_frm.doc.area){
+        cur_frm.set_value("full_address",
+        cur_frm.doc.province+', '
+        +cur_frm.doc.city+', '
+        +cur_frm.doc.area+', '
+        +cur_frm.doc.sub_area+', '
+        +cur_frm.doc.plate_no
+        );
+        cur_frm.refresh_field('full_address');
+        }
+    }
+    
+});
 
 
 frappe.ui.form.on("Project", "open_with_google_map", function(frm) {
     var myWin = window.open(cur_frm.doc.google_map_link);
+});
+
+frappe.ui.form.on('Project', {
+	onload(frm) {
+    	if(cur_frm.doc.google_map_link){
+    	    frm.set_df_property('open_with_google_map', "hidden", 0);
+    	}
+    	else{
+    	    frm.set_df_property('open_with_google_map', "hidden", 1);
+    	}
+    },
+    google_map_link(frm) {
+	    if(cur_frm.doc.google_map_link){
+    	    frm.set_df_property('open_with_google_map', "hidden", 0);
+    	}
+    	else{
+    	    frm.set_df_property('open_with_google_map', "hidden", 1);
+    	}
+	}
 });
