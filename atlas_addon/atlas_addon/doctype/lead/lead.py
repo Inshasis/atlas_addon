945132b856696cbe4b754bcf4bb7756329e79bc9
Hidayat ali <110670154+amafhhinfotech@users.noreply.github.com>
2023-04-29 14:44:30 +0530

    Update lead.py

--- atlas_addon/atlas_addon/doctype/lead/lead.py
+++ atlas_addon/atlas_addon/doctype/lead/lead.py
@@ -7,19 +7,34 @@ def after_insert(doc,method):
 	auto_create_address(doc)
 
 def auto_create_address(doc):
-	add = frappe.get_doc({
-		"doctype": "Address",
-		"address_title": doc.name,
-		"city": doc.citys,
-		"state": doc.province,
-		"address_line2": doc.areas,
-		"address_line1": doc.plate_no+"-"+doc.sub_area
-	})
-	add.append("links", {
-		'link_doctype': 'Lead',
-		'link_name': doc.name,
-		'link_title': doc.name,
-	})
+	if doc.sub_area:
+		add = frappe.get_doc({
+			"doctype": "Address",
+			"address_title": doc.name,
+			"city": doc.citys,
+			"state": doc.province,
+			"address_line2": doc.areas,
+			"address_line1": doc.plate_no+"-"+doc.sub_area
+		})
+		add.append("links", {
+			'link_doctype': 'Lead',
+			'link_name': doc.name,
+			'link_title': doc.name,
+		})
+		add.save()
 
-	add.save()
-	
+	else:
+		add = frappe.get_doc({
+			"doctype": "Address",
+			"address_title": doc.name,
+			"city": doc.citys,
+			"state": doc.province,
+			"address_line2": doc.areas,
+			"address_line1": doc.plate_no
+		})
+		add.append("links", {
+			'link_doctype': 'Lead',
+			'link_name': doc.name,
+			'link_title': doc.name,
+		})
+		add.save()
