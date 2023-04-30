c26acd05fc302dbf0d7cca1fe948fc631fe7cd43
Hidayat ali <110670154+amafhhinfotech@users.noreply.github.com>
2023-04-30 21:31:52 +0530

    Update sales_invoice.py

--- atlas_addon/atlas_addon/doctype/sales_invoice/sales_invoice.py
+++ atlas_addon/atlas_addon/doctype/sales_invoice/sales_invoice.py
@@ -7,3 +7,9 @@ import frappe
 def fetch_data(invoice_template):
 	items = frappe.db.sql(f""" select itt.item_group from `tabInvoice Template` it left join `tabInvoice Template Table` itt on itt.parent=it.name where it.name='{invoice_template}' ORDER BY itt.idx ASC;""",as_dict=1)
 	return items
+
+@frappe.whitelist() 
+def so_payment_terms_fetch(sales_order):
+	so_pay_terms = frappe.db.sql(f""" select payment_term,description,due_date,invoice_portion,payment_amount from `tabPayment Schedule` where parenttype='Sales Order' AND parent='{sales_order}' Order By idx ASC;""",as_dict=1)
+	return so_pay_terms
+
