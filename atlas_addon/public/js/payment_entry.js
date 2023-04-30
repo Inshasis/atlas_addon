dc94f386054e13658da1b4a25dede481ca0156cb
Hidayat ali <110670154+amafhhinfotech@users.noreply.github.com>
2023-04-30 21:31:03 +0530

    SI and Payment Entry JS

--- atlas_addon/public/js/payment_entry.js
+++ atlas_addon/public/js/payment_entry.js
@@ -11,3 +11,40 @@ frappe.ui.form.on('Payment Entry', {
         }
 	}
 });
+
+
+frappe.ui.form.on('Payment Entry', {
+  setup: function(frm, cdt, cdn){
+  // frm.set_df_property('sales_invoice', "hidden", 1);
+  var d = cur_frm.doc.references;
+  if(d[0].reference_doctype == "Sales Invoice"){
+      console.log(d[0].reference_name);    
+      frm.set_value("sales_invoice",d[0].reference_name);
+  }
+  }
+});
+
+
+frappe.ui.form.on('Payment Entry', {
+onload: function(frm){
+  if(frm.is_new() && cur_frm.doc.sales_invoice){
+  frappe.call({
+  method: 'atlas_addon.atlas_addon.doctype.payment_entry.payment_entry.si_payment_terms_fetch',
+  args: {sales_invoice: cur_frm.doc.sales_invoice}
+  }).done((r) => {
+      console.log(r);
+      cur_frm.clear_table("references");
+      $.each(r.message, function(_i, e){
+      let entry = frm.add_child("references");
+      entry.reference_doctype = "Sales Invoice";
+      entry.reference_name = cur_frm.doc.sales_invoice;
+      entry.due_date = e.due_date;
+      entry.payment_term = e.payment_term;
+      entry.allocated_amount = e.payment_amount;
+      });
+      refresh_field("references");
+
+  });
+  }
+  }
+});
--- atlas_addon/public/js/sales_invoice.js
+++ atlas_addon/public/js/sales_invoice.js
@@ -75,5 +75,38 @@ frappe.ui.form.on('Sales Invoice', {
         }
 });
 
+//Payment Terms 
+frappe.ui.form.on('Sales Invoice', {
+    setup: function(frm, cdt, cdn){
+    frm.set_df_property('sales_order', "hidden", 1);
+    var d = cur_frm.doc.items;
+    console.log(d[0].sales_order);
+    frm.set_value("sales_order",d[0].sales_order);
+    }
+});
 
+
+frappe.ui.form.on('Sales Invoice', {
+  onload: function(frm){
+    if(frm.is_new() && cur_frm.doc.sales_order){
+    frappe.call({
+    method: 'atlas_addon.atlas_addon.doctype.sales_invoice.sales_invoice.so_payment_terms_fetch',
+    args: {sales_order: cur_frm.doc.sales_order}
+    }).done((r) => {
+        console.log(r);
+        $.each(r.message, function(_i, e){
+        let entry = frm.add_child("payment_schedule");
+        entry.payment_term = e.payment_term;
+        entry.description = e.description;
+        entry.due_date = e.due_date;
+        entry.invoice_portion = e.invoice_portion;
+        entry.payment_amount = e.payment_amount;
+        
+        });
+        refresh_field("payment_schedule");
+
+    });
+    }
+    }
+});
 
